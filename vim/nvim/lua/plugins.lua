008760e9cd9997182511b2a7e43d355b032dac8f
liujia <liujia@taptap.com>
2022-12-23 21:16:43 +0800

    (vim): new vim theme

---
+++ vim/nvim/lua/plugins.lua
@@ -0,0 +1,83 @@
+
+require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({with_sync=true})
+            ts_update()
+        end,
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use 'shaunsingh/nord.nvim'
+
+
+end 
+)
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "help","c","cpp","lua", "rust" ,"java","javascript","scala","python","go"},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+
+
+  highlight = {
+    enable = true,
+  },
+}
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
--- vim/nvim/lua/settings.lua
+++ vim/nvim/lua/settings.lua
@@ -6,4 +6,5 @@ vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.number = true
 vim.o.background = "dark"
-vim.cmd([[colorscheme gruvbox]])
+vim.o.termguicolors = true
+vim.cmd([[colorscheme nord]])
