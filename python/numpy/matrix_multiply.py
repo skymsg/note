d80f2e8ac633a69381c7b56117965583e84eeb11
skymsg <infoflow@163.com>
2022-12-31 01:34:15 +0800

    (english): numpy matrix multiply

--- python/numpy/matrix_multiply.py
+++ python/numpy/matrix_multiply.py
@@ -1,6 +1,6 @@
 import numpy as np
-m = np.random.randint(0,500,size = (20000,20000))
-n = np.random.randint(0,500,size = (20000,20000))
-z = m*n
+m = np.random.randint(0,500,size = (200,201))
+n = np.random.randint(0,500,size = (201,20000))
+z = np.matmul(m,n)
 print(z)
 
